1abcd2a6ef5bec5f8dff8e5a0b4a30304cdb88d0
ahmedmido2000 <aa1536198@gmail.com>
2024-04-23 17:24:03 +0200

    up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CardDetails from './Components/CardDetails';
 import HomePage from './HomePage';
--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './NavBar.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { NavDropdown } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp, faTelegram, faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -8,28 +11,26 @@ const NavBar = () => {
   return (
     <div>
       <header>
-      <nav className="navbar position-fixed navbar-expand-lg bg-light w-100">
-                <div className="container">
-                <a href="#" className="navbar-brand text-primary" id="brand">سكن قنا</a>
-                <a href="#x" data-toggle="collapse" className="navbar-toggler">
-                <FontAwesomeIcon icon={faBars} id="nav-icon" />
-                </a>
-                <div className="collapse navbar-collapse" id="x">
-                    <ul className="navbar-nav m-auto mb-2 mb-lg-0">
-                        <li className="nav-item p-1 active"><a href="index.php" className="nav-link text-primary">الرئيسية</a></li>
-                        <li className="nav-item p-1"><a href="#category" className="nav-link text-primary">من نحن </a></li>
-                        <li className="nav-item p-1"><a href="#category" className="nav-link text-primary">العقارات</a></li>
-                        <li className="nav-item p-1 "><a href="#footer" className="nav-link text-primary">اتصل بنا</a></li>
-                    </ul>
-                </div>      
-                <div className="links">
-                <a target='_blank' href="https://api.whatsapp.com/send/?phone=201155170824&text&app_absent=0"><FontAwesomeIcon icon={faWhatsapp}  /></a>
-                <a target='_blank' href="https://t.me/Ahmed17112"><FontAwesomeIcon icon={faTelegram}  /></a>
-                <a target='_blank' href="https://www.facebook.com/profile.php?id=100010113310839&mibextid=ZbWKwL"><FontAwesomeIcon icon={faFacebook}  /></a>
-                <a target='_blank' href="mailto:aa1536198@gmail.com"><FontAwesomeIcon icon={faEnvelope}  /></a>
-                </div>      
-            </div>
-            </nav>
+      <Navbar bg="light" expand="lg">
+      <div className="container">
+        <Navbar.Brand href="#" className="text-primary">سكن قنا</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="m-auto">
+            <Nav.Link href="index.php" className="text-primary">الرئيسية</Nav.Link>
+            <Nav.Link href="#category" className="text-primary">من نحن</Nav.Link>
+            <Nav.Link href="#category" className="text-primary">العقارات</Nav.Link>
+            <Nav.Link href="#footer" className="text-primary">اتصل بنا</Nav.Link>
+          </Nav>
+        <div className="links">
+          <a target='_blank' href="https://api.whatsapp.com/send/?phone=201155170824&text&app_absent=0"><FontAwesomeIcon icon={faWhatsapp} /></a>
+          <a target='_blank' href="https://t.me/Ahmed17112"><FontAwesomeIcon icon={faTelegram} /></a>
+          <a target='_blank' href="https://www.facebook.com/profile.php?id=100010113310839&mibextid=ZbWKwL"><FontAwesomeIcon icon={faFacebook} /></a>
+          <a target='_blank' href="mailto:aa1536198@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a>
+        </div>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
       </header>
     </div>
   )
